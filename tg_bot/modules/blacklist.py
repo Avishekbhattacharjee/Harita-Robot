d01321a92b54658e77ad9f60c4db57286513e888
Avishekbhattacharjee <52344359+Avishekbhattacharjee@users.noreply.github.com>
2020-06-27 00:11:03 +0530

    Update blacklist.py

--- tg_bot/modules/blacklist.py
+++ tg_bot/modules/blacklist.py
@@ -182,5 +182,5 @@ dispatcher.add_handler(ADD_BLACKLIST_HANDLER)
 dispatcher.add_handler(UNBLACKLIST_HANDLER)
 dispatcher.add_handler(BLACKLIST_DEL_HANDLER, group=BLACKLIST_GROUP)
 
-__mod_name__ = "Word Blacklists"
+__mod_name__ = "Blacklists"
 __handlers__ = [BLACKLIST_HANDLER, ADD_BLACKLIST_HANDLER, UNBLACKLIST_HANDLER, (BLACKLIST_DEL_HANDLER, BLACKLIST_GROUP)]
