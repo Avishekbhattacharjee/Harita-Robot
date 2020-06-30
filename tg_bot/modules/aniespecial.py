ce9d3dd777314641b69c0fd1485e8485c5cf043c
Avishekbhattacharjee <52344359+Avishekbhattacharjee@users.noreply.github.com>
2020-06-30 12:44:03 +0530

    Create aniespecial.py

---
+++ tg_bot/modules/aniespecial.py
@@ -0,0 +1,20 @@
+__help__ = """
+- /time <query> : Gives information about a timezone.
+(Available queries : Country Code/Country Name/Timezone Name)
+- /shout <keyword>: write anything you want to give loud shout.
+- /toss Play Toss by using this command.
+- /lyrics <song>: returns the lyrics of that song.
+- /tr (language code) as reply to a long message
+- /weather {Area Name} :- Find your area weather by using this command.
+- /zal type some text 
+- /git:{GitHub username} Returns info about a GitHub user or organization.
+- /music <Youtube link> : download audio file from youtube link
+- /import: reply to a group butler backup file to import as much as possible, making the transfer super simple! Note \
+that files/photos can't be imported due to telegram restrictions.
+- /info {userid}:- get information of a userv by using this.
+- /banip {userid}:- you can ban a user ip , the user can't join your groups by using random accounts where the bot present. 
+- /scan :- Reply to a document , bot will scan your document like scanner(Comming Soon)
+- /google <text>: perform a google search(⚠️Not Working Now, Soon Work⚠️)
+- /sudolist :- See The Sudolist if you need any help.
+"""
+__mod_name__ = "✳️Special✳️"
