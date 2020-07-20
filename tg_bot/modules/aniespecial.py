51590dc8042940fde620675da8a33a2b20852c60
Avishekbhattacharjee <52344359+Avishekbhattacharjee@users.noreply.github.com>
2020-07-20 23:55:18 +0530

    Update aniespecial.py

--- tg_bot/modules/aniespecial.py
+++ tg_bot/modules/aniespecial.py
@@ -1,10 +1,16 @@
 __help__ = """
+🌟Here You get a list of special commands. Let's Enjoy this. For Any bugs or support join @AnieSupport 🔥
+
 - /time <query> : Gives information about a timezone.
 (Available queries : Country Code/Country Name/Timezone Name)
 - /cash : currency converter [example syntax: /cash 1 USD INR]
 - /shout <keyword>: write anything you want to give loud shout.
 - /weebify <text>: returns a weebified text.
+- Reply to a text with /insult for insults.
+- /wiki text: Returns search from wikipedia for the input text
 - /toss Play Toss by using this command.
+- /ud <word>: Type the word or expression you want to search use.
+- /urban <word>: Same as /ud
 - /blackout <text>: Apply Blackout Style to your text
 - /lyrics <song>: returns the lyrics of that song.
 - /tr (language code) as reply to a long message
@@ -18,6 +24,7 @@ __help__ = """
 - /import: reply to a group butler backup file to import as much as possible, making the transfer super simple! Note \
 that files/photos can't be imported due to telegram restrictions.
 - /info {userid}:- get information of a userv by using this.
+-/whois {username} :- 
 - /banip {userid}:- you can ban a user ip , the user can't join your groups by using random accounts where the bot present. 
 - /scan :- Reply to a document , bot will scan your document like scanner(Comming Soon)
 - /google <text>: perform a google search(⚠️Not Working Now, Soon Work⚠️)
