849887870c031833e94bab910f30bcff127f627d
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-09-16 10:20:42 +0530

    Update __init__.py

--- tg_bot/modules/sql/__init__.py
+++ tg_bot/modules/sql/__init__.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
 
 from tg_bot import DB_URI
 
