bd1ec371fb70de03f3626fe4f11798104f14fa11
Avishekbhattacharjee <52344359+Avishekbhattacharjee@users.noreply.github.com>
2020-09-28 16:41:11 +0530

    Update rules.py

--- tg_bot/modules/rules.py
+++ tg_bot/modules/rules.py
@@ -49,8 +49,6 @@ def send_rules(update, chat_id, from_pm=False):
         update.effective_message.reply_text("The group admins haven't set any rules for this chat yet. "
                                             "This probably doesn't mean it's lawless though...!")
         
-   if user_can_changeinfo(chat, user, context.bot.id) == False: message.reply_text("You are missing the following rights to use this command:\n" "CanChangeInfo") return ""
-
 @run_async
 @user_admin
 def set_rules(bot: Bot, update: Update):
