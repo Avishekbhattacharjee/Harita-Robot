6bc96a52404f42d616c269c228c08557d0dd2b64
Avishekbhattacharjee <52344359+Avishekbhattacharjee@users.noreply.github.com>
2020-09-29 23:38:38 +0530

    Update alternate.py

--- tg_bot/modules/helper_funcs/alternate.py
+++ tg_bot/modules/helper_funcs/alternate.py
@@ -1,18 +1,39 @@
-import sys
-import traceback
-
 from functools import wraps
-from typing import Optional
-
-from telegram import User, Chat, ChatMember, Update, Bot
-from telegram import error
-
-from tg_bot import DEL_CMDS, SUDO_USERS, WHITELIST_USERS
+from telegram import error, ChatAction
 
 
-def send_message(message, text,  *args,**kwargs):
-	try:
-		return message.reply_text(text, *args,**kwargs)
-	except error.BadRequest as err:
-		if str(err) == "Reply message not found":
-			return message.reply_text(text, quote=False, *args,**kwargs)
+def send_message(message, text, *args, **kwargs):
+    try:
+        return message.reply_text(text, *args, **kwargs)
+    except error.BadRequest as err:
+        if str(err) == "Reply message not found":
+            return message.reply_text(text, quote=False, *args, **kwargs)
+
+
+def typing_action(func):
+    """Sends typing action while processing func command."""
+
+    @wraps(func)
+    def command_func(update, context, *args, **kwargs):
+        context.bot.send_chat_action(
+            chat_id=update.effective_chat.id, action=ChatAction.TYPING
+        )
+        return func(update, context, *args, **kwargs)
+
+    return command_func
+
+
+def send_action(action):
+    """Sends `action` while processing func command."""
+
+    def decorator(func):
+        @wraps(func)
+        def command_func(update, context, *args, **kwargs):
+            context.bot.send_chat_action(
+                chat_id=update.effective_chat.id, action=action
+            )
+            return func(update, context, *args, **kwargs)
+
+        return command_func
+
+    return decorator
